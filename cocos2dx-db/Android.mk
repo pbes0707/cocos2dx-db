9277b2b69e5471409f77f1604164f0be65052123
luma <stubma@gmail.com>
2013-03-12 16:57:33 +0800

    refactor common code to cocos2dx-common

--- cocos2dx-db/Android.mk
+++ cocos2dx-db/Android.mk
@@ -18,14 +18,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := cocos2dx-db
-LOCAL_SRC_FILES := $(call all-cpp-files-under,../src)
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include \
-                    $(LOCAL_PATH)/../include/platform/android
-LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/system_libs/$(TARGET_ARCH) -lsqlite -llog
+LOCAL_SRC_FILES := $(call all-cpp-files-under,src)
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include \
+                    $(LOCAL_PATH)/include/platform/android
+LOCAL_EXPORT_LDLIBS := -L$(LOCAL_PATH)/system_libs/$(TARGET_ARCH) -lsqlite
 LOCAL_C_INCLUDES := $(LOCAL_EXPORT_C_INCLUDES)
 LOCAL_LDLIBS := $(LOCAL_EXPORT_LDLIBS)
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocos2dx-common
 
 include $(BUILD_STATIC_LIBRARY)
 
 $(call import-module,cocos2dx)
+$(call import-module,cocos2dx-common)
